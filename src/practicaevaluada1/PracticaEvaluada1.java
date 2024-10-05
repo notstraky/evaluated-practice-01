92df1c914b9959bc634ee9aad83c652338f80239
Sylxnt <justsylent@gmail.com>
2024-10-05 00:42:14 -0600

    Update validation for Exercise 3: Ensure number is between 1 and 10 for multiplication table

--- src/practicaevaluada1/PracticaEvaluada1.java
+++ src/practicaevaluada1/PracticaEvaluada1.java
@@ -17,52 +17,55 @@ public class PracticaEvaluada1 {
      */
     public static void main(String[] args) {
         // Ejercicio 1: Números Pares e Impares
-        int num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero entero positivo"));
-
-        int i = 1;
-        while (i <= num) {
-            if (i % 2 == 0) {
-                JOptionPane.showConfirmDialog(null, "el numero " + i
-                        + " es par");
-            } else {
-                JOptionPane.showConfirmDialog(null, "el numero " + i
-                        + " es impar");
-
-            }
-            i++;
-        }
-
-        // Ejercicio 2: Suma de Números
-        
-        int n;
-        
-        while (true){
-            n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero positivo o un negativo para salir"));
-            
-            if(n<0) {
-                break;
-            }
-            
-            int sumador = 0;
-            for (int j = 1; j<=n;j++){
-                sumador +=j;
-            }
-            
-             JOptionPane.showMessageDialog(null, "La suma de los números desde 1 hasta " + n + " es: " + sumador);
-        }
-        
+//        int num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero entero positivo"));
+//
+//        int i = 1;
+//        while (i <= num) {
+//            if (i % 2 == 0) {
+//                JOptionPane.showConfirmDialog(null, "el numero " + i
+//                        + " es par");
+//            } else {
+//                JOptionPane.showConfirmDialog(null, "el numero " + i
+//                        + " es impar");
+//
+//            }
+//            i++;
+//        }
+//
+//        // Ejercicio 2: Suma de Números
+//        
+//        int n;
+//        
+//        while (true){
+//            n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero positivo o un negativo para salir"));
+//            
+//            if(n<0) {
+//                break;
+//            }
+//            
+//            int sumador = 0;
+//            for (int j = 1; j<=n;j++){
+//                sumador +=j;
+//            }
+//            
+//             JOptionPane.showMessageDialog(null, "La suma de los números desde 1 hasta " + n + " es: " + sumador);
+//        }
+//        
         // Ejercicio 3: Tablas de Multiplicar
-        
-        int numTabla = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero del 1 al 10 para obtener su tabla de multiplicar"));
-        
-        if (numTabla > 0 && numTabla <=10) {
-            for(int k = 1; k<=10; k++) {
-                // El profe permitió usar print para el ejercicio de la tabla de multiplicar.
-                System.out.println(numTabla + " x " +i + " es: " + (i*numTabla)); 
+        while (true) {
+            int numTabla = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero del 1 al 10 para obtener su tabla de multiplicar"));
+
+            if (numTabla > 0 && numTabla <= 10) {
+                for (int k = 1; k <= 10; k++) {
+                    // El profe permitió usar print para el ejercicio de la tabla de multiplicar.
+                    System.out.println(numTabla + " x " + k + " es: " + (k * numTabla));
+                }
+                break;
+            } else{
+                JOptionPane.showConfirmDialog(null, "Error el numero ingresado '"+ numTabla + "' no esta en el rango de 1 a 10");
             }
         }
-        
-        
+
     }
 
 }
